de40d411d8f0fb962a7f8e19cd976bc6c9919294
Leonan Lódula (Shacalliitto) <84084794+Shacalliitto@users.noreply.github.com>
2022-04-03 13:45:16 -0300

    first commit

---
+++ clock.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from time import strftime
+
+def tic():
+    rel['text']=strftime('%H:%M:%S')
+
+def tac():
+    tic()
+    rel.after(1000, tac)
+
+j = Tk()
+
+rel = Label(j, font='arial 30')
+rel.pack()
+tac()
+
+j.mainloop()
